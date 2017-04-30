a307c3bac0b85e58ea92856107ffee749b54513d
AZCoder2 <tranalli2@gmail.com>
2017-04-30 15:04:08 -0700

    Part 1 & 2 from durul's tut.

--- Combat/ViewController.swift
+++ Combat/ViewController.swift
@@ -9,17 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let theme = ThemeManager.currentTheme()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = theme.backgroundColor
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
-
--- DRL Theme Manager.swift
+++ DRL Theme Manager.swift
@@ -5,24 +5,28 @@
 // Copyright (c) 2017 Latitude23
 //
 
+//
+//  ThemeManager.swift
 import UIKit
-import XCPlayground
 import Foundation
 
+/// Enum Theme Manager
+/// - Note: https://github.com/durul/DRL-Theme-Manager
 enum Theme: Int {
-
+    
     case Dark, Graphical
-
+    
+    /// Main Color
     var mainColor: UIColor {
         switch self {
-        case .Dark:
-            return UIColor(red: 242.0/255.0, green: 101.0/255.0, blue: 34.0/255.0, alpha: 1.0)
         case .Graphical:
-            return UIColor(red: 10.0/255.0, green: 10.0/255.0, blue: 10.0/255.0, alpha: 1.0)
+            return UIColor(red: 242.0/255.0, green: 101.0/255.0, blue: 34.0/255.0, alpha: 1.0)
+        case .Dark:
+            return UIColor(red:0.13, green:0.13, blue:0.17, alpha:1.0)
         }
     }
-
-    //Customizing the Navigation Bar
+    
+    /// Customizing the Navigation Bar
     var barStyle: UIBarStyle {
         switch self {
         case .Graphical:
@@ -31,41 +35,47 @@ enum Theme: Int {
             return .black
         }
     }
-
-    var navigationBackgroundImage: UIImage? {
-        return self == .Graphical ? UIImage(named: "navBackground") : nil
-    }
-
-    var tabBarBackgroundImage: UIImage? {
-        return self == .Graphical ? UIImage(named: "tabBarBackground") : nil
-    }
-
+    
+    /// BackgroundColor properties
     var backgroundColor: UIColor {
-        switch self {
-        case .Graphical:
-            return UIColor(white: 0.9, alpha: 1.0)
-        case .Dark:
-            return UIColor(white: 0.8, alpha: 1.0)
-        }
+        return UIColor(red:0.13, green:0.13, blue:0.17, alpha:1.0)
     }
-
-    var secondaryColor: UIColor {
-        switch self {
-        case .Dark:
-            return UIColor(red: 34.0/255.0, green: 128.0/255.0, blue: 66.0/255.0, alpha: 1.0)
-        case .Graphical:
-            return UIColor(red: 140.0/255.0, green: 50.0/255.0, blue: 48.0/255.0, alpha: 1.0)
-        }
+    
+    /// MainLabelColor properties
+    var mainLabelColor: UIColor {
+        return UIColor(red:0.58, green:0.78, blue:0.44, alpha:1.0)
+    }
+    
+    /// CommentColor properties
+    var commentColor: UIColor {
+        return UIColor(red:0.61, green:0.64, blue:0.69, alpha:1.0)
+    }
+    
+    /// BlueColor properties
+    var blueColor: UIColor {
+        return UIColor(red:0.27, green:0.44, blue:0.85, alpha:1.0)
+    }
+    
+    /// OrangeColor properties
+    var orangeColor: UIColor {
+        return  UIColor(red:0.82, green:0.56, blue:0.36, alpha:1.0)
+    }
+    
+    /// FuchsiaColor
+    var fuchsiaColor: UIColor {
+        return  UIColor(red:0.72, green:0.22, blue:0.60, alpha:1.0)
     }
 }
 
-// Enum declaration
+/// Enum declaration
 let SelectedThemeKey = "SelectedTheme"
 
-// This will let you use a theme in the app.
+/// This will let you use a theme in the app.
 struct ThemeManager {
-
-    // ThemeManager
+    
+    /// currentTheme: static function store current theme.
+    /// Returns: Theme
+    /// Parameters: None
     static func currentTheme() -> Theme {
         if let storedTheme = (UserDefaults.standard.value(forKey: SelectedThemeKey) as AnyObject).integerValue {
             return Theme(rawValue: storedTheme)!
@@ -73,51 +83,22 @@ struct ThemeManager {
             return .Graphical
         }
     }
-
+    
+    /// applyTheme: static function applyTheme.
+    /// Returns: None
+    /// Parameters: theme
     static func applyTheme(theme: Theme) {
         // First persist the selected theme using NSUserDefaults.
         UserDefaults.standard.setValue(theme.rawValue, forKey: SelectedThemeKey)
         UserDefaults.standard.synchronize()
-
+        
         // You get your current (selected) theme and apply the main color to the tintColor property of your application’s window.
         let sharedApplication = UIApplication.shared
         sharedApplication.delegate?.window??.tintColor = theme.mainColor
-
-        UINavigationBar.appearance().barStyle = theme.barStyle
-        UINavigationBar.appearance().setBackgroundImage(theme.navigationBackgroundImage, for: .default)
-        UINavigationBar.appearance().backIndicatorImage = UIImage(named: "backArrow")
-        UINavigationBar.appearance().backIndicatorTransitionMaskImage = UIImage(named: "backArrowMaskFixed")
-
-        UITabBar.appearance().barStyle = theme.barStyle
-        UITabBar.appearance().backgroundImage = theme.tabBarBackgroundImage
-
-        let tabIndicator = UIImage(named: "tabBarSelectionIndicator")?.withRenderingMode(.alwaysTemplate)
-        let tabResizableIndicator = tabIndicator?.resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 2.0, bottom: 0, right: 2.0))
-        UITabBar.appearance().selectionIndicatorImage = tabResizableIndicator
-
-        let controlBackground = UIImage(named: "controlBackground")?.withRenderingMode(.alwaysTemplate)
-            .resizableImage(withCapInsets: UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3))
-        let controlSelectedBackground = UIImage(named: "controlSelectedBackground")?
-            .withRenderingMode(.alwaysTemplate)
-            .resizableImage(withCapInsets: UIEdgeInsets(top: 3, left: 3, bottom: 3, right: 3))
-
-        UISegmentedControl.appearance().setBackgroundImage(controlBackground, for: .normal, barMetrics: .default)
-        UISegmentedControl.appearance().setBackgroundImage(controlSelectedBackground, for: .selected, barMetrics: .default)
-
-        UIStepper.appearance().setBackgroundImage(controlBackground, for: .normal)
-        UIStepper.appearance().setBackgroundImage(controlBackground, for: .disabled)
-        UIStepper.appearance().setBackgroundImage(controlBackground, for: .highlighted)
-        UIStepper.appearance().setDecrementImage(UIImage(named: "fewerPaws"), for: .normal)
-        UIStepper.appearance().setIncrementImage(UIImage(named: "morePaws"), for: .normal)
-
-        UISlider.appearance().setThumbImage(UIImage(named: "sliderThumb"), for: .normal)
-        UISlider.appearance().setMaximumTrackImage(UIImage(named: "maximumTrack")?
-            .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 0.0, bottom: 0, right: 6.0)), for: .normal)
-        UISlider.appearance().setMinimumTrackImage(UIImage(named: "minimumTrack")?
-            .withRenderingMode(.alwaysTemplate)
-            .resizableImage(withCapInsets: UIEdgeInsets(top: 0, left: 6.0, bottom: 0, right: 0)), for: .normal)
-
-        UISwitch.appearance().onTintColor = theme.mainColor.withAlphaComponent(0.3)
-        UISwitch.appearance().thumbTintColor = theme.mainColor
+        
+        UINavigationBar.appearance().barTintColor = theme.backgroundColor
+        UINavigationBar.appearance().tintColor = UIColor.white
+        UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName : theme.blueColor]
+        
     }
 }
